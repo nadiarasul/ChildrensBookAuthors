08dfa933ceadfb4ba034de5541c6667290167360
nadiarasul <nadia.rasul@gmail.com>
2016-05-29 16:30:42 -0400

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,242 @@
+'use strict';
+
+var works = [{
+    isbn: '0888994168',
+    img: 'images/de_The_Breadwinner.jpg'
+}, {
+    isbn: '0888995199',
+    img: 'images/de_Parvanas_journey.jpg'
+}, {
+    isbn: '0888995423',
+    img: 'images/de_Mud_City.jpg'
+}, {
+    isbn: '1554982979',
+    img: 'images/de_My_Name_Is_Parvana.jpg'
+}, {
+    isbn: '0888999593',
+    img: 'images/de_The_Breadwinner_Trilogy.jpg'
+}, {
+    isbn: '0888997353',
+    img: 'images/de_I_Am_A_Taxi.jpg'
+}, {
+    isbn: '0060531827',
+    img: 'images/ko_Airborn.jpg'
+}, {
+    isbn: '0006481795',
+    img: 'images/ko_Silverwing.jpg'
+}, {
+    isbn: '0060532297',
+    img: 'images/ko_skybreaker.jpg'
+}, {
+    isbn: '1442403152',
+    img: 'images/ko_this_dark_endeavor.jpg'
+}, {
+    isbn: '000648171X',
+    img: 'images/ko_Sunwing.jpg'
+}, {
+    isbn: '1554681804',
+    img: 'images/vg_not_suitable.jpg'
+}, {
+    isbn: '1551435667',
+    img: 'images/vg_dead_end_job.jpg'
+}, {
+    isbn: '1551436434',
+    img: 'images/vg_pigboy.jpg'
+}, {
+    isbn: '1554681820',
+    img: 'images/vg_betsy_wickwire_.jpg'
+}, {
+    isbn: '1551433702',
+    img: 'images/vg_quid_pro_quo.jpg'
+}, {
+    isbn: '1551436949',
+    img: 'images/vg_id.jpeg'
+}, {
+    isbn: '1770494820',
+    img: 'images/cy_Nancy_Knows.jpg'
+}, {
+    isbn: '1554535689',
+    img: 'images/cy_Ten_Birds.jpg'
+}, {
+    isbn: '088899995X',
+    img: 'images/cy_A_Few_Blocks.jpg'
+}, {
+    isbn: '1554983398',
+    img: 'images/cy_Some_Things_We_Lost.jpg'
+}, {
+    isbn: '1894786602',
+    img: 'images/cy_The_Queens_Shadow.jpg'
+}, {
+    isbn: '1554982952',
+    img: 'images/cy_A_Few_Bites.jpg'
+}, {
+    isbn: '0888994044',
+    img: 'images/Stella_Queen_Of_The_Snow.jpg'
+}, {
+    isbn: '0888995725',
+    img: 'images/mlg_Stella_Star_Of_The_Sea.jpg'
+}, {
+    isbn: '1554983827',
+    img: 'images/mlg_Any_Questions.jpg'
+}, {
+    isbn: '0888997108',
+    img: 'images/mlg_Stella_Fairy_Of_The_Forest.jpg'
+}, {
+    isbn: '0888999062',
+    img: 'images/mlg_When_Stells_Was_Very_Very_Small.jpg'
+}, {
+    isbn: '0888999941',
+    img: 'images/mlg_Roslyn_Ruabaga_And_The_Biggest_Hole.jpg'
+}];
+
+var authors = [{
+    name: 'Deborah Ellis',
+    id: '95863',
+    dob: '1960/01/01',
+    hometown: 'Cochrane, Ontario',
+    bio: '<p>A renowned author, feminist, and peace activist, Deborah Ellis frequently tackles issues of nationalism, poverty and sexism in her writings. The author of the international  best-selling The Breadwinner, Ellis has donated more than one million in royalties to Canadian Women for Women in Afghanistan, Street Kids International, the Children in Crisis Fund of IBBY (International Board on Books for Young People) and UNICEF.</p><p>She has won the Governor General’s Award, the Ruth Schwartz Award, the University of California’s Middle East Book Award, Sweden’s Peter Pan Prize, the Jane Addams Children’s Book Award, and the Vicky Metcalf Award for a Body of Work, and been named to the Order of Ontario.</p>'
+
+}, {
+    name: 'Kenneth Oppel',
+    id: '88922',
+    dob: '1967/08/31',
+    hometown: 'Port Alberni, British Columbia',
+    bio: '<p>At the age of fifteen, Kenneth Oppel’s first book was recommended for publication by his favourite author, Roald Dahl. “I never had any direct correspondence with Dahl myself,” he said in 2013. “Though I do have a letter he wrote to our friend, referring to me as a “teenaged novelist genius” which really was very in keeping with his tendency for hyperbole.”</p><p>Since then, the Port Alberni, British Columbia native has gone on to collect the 2004 Governor General’s Literary Award for English language children’s literature as well as  numerous recognitions from the American Library Association. His Silverwing trilogy, about the adventures of a group of migrating bats, has sold over one million books across the world.</p><p>The author of nineteen books, Oppel currently lives in Toronto with his wife, and three children.</p> '
+}, {
+    name: 'Vickie Grant',
+    id: '611315',
+    dob: '',
+    hometown: 'Halifax, Nova Scotia',
+    bio: '<p>Vicki Grant has been called “a superb storyteller” (Canadian Children’s Book News) with “a wonderful sense of comedy” (CM Magazine) and “one of the funniest writers working today” (Vancouver Sun).</p><p>The author of fourteen books, her works have been translated into seven languages, and shortlisted for numerous awards in both Canada and the United States. Grant has also written almost 100 episode of children’s television, and is set to begin work on her third documentary.</p><p>She lives in Halifax, Nova Scotia.</p>'
+}, {
+    name: 'Cybèle Young',
+    id: '4625304',
+    dob: '',
+    hometown: 'Toronto Ontario',
+    bio: '<p>A Toronto-based artist and author, Cybèle Young’s work draws heavily on her illustration and storytelling strengths. Since graduating from sculpture and printmaking at the Ontario College of Art and Design in 1995, Young\'s artwork has appeared in galleries around the world, such as New York, LA, Miami, London, Stockholm, Japan, Singapore, Korea and across Canada. Her 2011 release ‘Ten Birds’ won the Governor General’s Award for Illustration  in 2011.</p><p>"I fell in love with children’s books when I was pregnant,” she told Quill & Quire in 2012. “And as a parent, there’s nothing more heavenly than knowing your kid, who could be climbing the walls, will sit happily in your lap if you offer them a book, and you can both be transported to another world."</p>'
+}, {
+    name: 'Marie-Louise Gay',
+    id: '80974',
+    dob: '1952/06/17',
+    hometown: 'Quebec City, Quebec',
+    bio: '<p>One of Canada’s most honoured authors and illustrators, Mary-Louise Gay has published numerous books in both French and English. Her famed Stella and Sam series has been published in more than twelve languages, and was recently developed into a 52-episode cartoon series. For her work, she has won numerous awards and prizes.</p><p>"Children are so open," Gay told the University of Manitoba in 1988, "and their link between fantasy and reality is so fragile. I\'m always amazed at what they will and won\'t believe. They have no problem accepting that a polar bear might come out of the refrigerator, but then they tell me that everyone knows that you don\'t keep bananas in the refrigerators."</p><p>Gay currently lives in Montreal.</p>'
+
+}];
+
+var bookTitle;
+
+var childrenApp = {};
+var idc = '';
+
+childrenApp.init = function () {
+    //Function to make ajax calls when author image or button are clicked
+    $('.authorsClick, button').on('click', function (e) {
+        e.preventDefault();
+        $('#main').hide();
+        $('#content').show();
+        $('.bookGallery').empty();
+        $('button').removeClass('active');
+        //get author ID which is stored in data- attribute on the button to make the ajax call
+        var authorId = $(e.currentTarget).data('author-id');
+        childrenApp.getAuthor(authorId);
+    });
+};
+
+var apiKey = '0MVd0sx7a0AVeLLDN8lPXg';
+
+//Pass author ID that we got from data- attribute to make the ajax call for each author
+childrenApp.getAuthor = function (authorId) {
+    $.ajax({
+        url: 'http://proxy.hackeryou.com',
+        dataType: 'json',
+        method: 'GET',
+        data: {
+            reqUrl: 'https://www.goodreads.com/author/show.xml',
+            params: {
+                key: apiKey,
+                id: authorId
+            },
+            xmlToJSON: true
+        }
+    }).then(function (res) {
+        childrenApp.displayInfo(res.GoodreadsResponse.author);
+    });
+};
+
+// Get book image URL from the API
+childrenApp.getBookUrl = function (book) {
+    var match = works.filter(function (work) {
+        return work.isbn === book.isbn;
+    });
+    if (match.length > 0) {
+        return match[0].img;
+    } else {
+        return book.image_url;
+    }
+};
+
+childrenApp.displayInfo = function (authorInfo) {
+    var bookReturn = authorInfo.books.book;
+    var loopLength = 6;
+    if (bookReturn.length < 6) {
+        loopLength = bookReturn.length;
+    }
+    for (var i = 0; i < loopLength; i++) {
+        if (bookReturn[i].description.length !== undefined) {
+            bookTitle = $('<h2>').html(bookReturn[i].title);
+            var bookDescription = $('<p>').html(cleanup(bookReturn[i].description));
+            var theDesc = cleanup(bookReturn[i].description);
+            var bookImageUrl = childrenApp.getBookUrl(bookReturn[i]);
+            var bookImage = $('<img>').attr('src', bookImageUrl).data('desc', theDesc).data('name', bookTitle);
+            $('.bookName').html(bookTitle);
+            $('.bookDescription').html(bookDescription);
+            $('.bookGallery').append(bookImage);
+            addImgListener();
+        }
+    }
+    $('button[data-author-id=' + authorInfo.id + ']').addClass('active');
+    var authorHometown = authorInfo.hometown;
+    var authorLink = authorInfo.link;
+    var authorBirthday = authorInfo.born_at;
+    var authorName = $('<h2>').text(authorInfo.name);
+    var authorID = authorInfo.id;
+    var authorBioMatch = authors.filter(function (author) {
+        return author.id === authorID;
+    });
+    $('.readMore').attr('href', authorLink);
+    $('.authorName').html(authorName);
+    $('.birthday').text(authorBioMatch[0].dob);
+    $('.hometown').text(authorBioMatch[0].hometown);
+    $('.authorBio').html(authorBioMatch[0].bio);
+};
+// Regex to clean up the description that we got back from the API
+var cleanup = function cleanup(string) {
+    return string.replace(/&#40;/g, "(").replace(/&#41;/g, ")").replace(/&#35;/g, "#").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/<br>/g, "").replace(/<\/em>/g, "").replace(/&apos;/g, "'").replace(/<em>/g, "").replace(/&quot;/g, "").replace(/<p>/g, "").replace(/<\/p>/g, "").replace(/<a href>/g, "").replace(/<\/a>/g, "").replace(/<blockquote>/g, "").replace(/<\/blockquote>/g, "").replace(/<strong>/g, "").replace(/<\/strong>/g, "").replace(/&amp;amp;/g, "&").replace(/&amp;/g, "&").replace(/\s\s\s\s\s\s\s\s/, ' ');
+};
+
+// Image listener to get the book description stored in data- attribute on the corresponding image
+var addImgListener = function addImgListener() {
+    $('.bookGallery img').on('click', function () {
+        var description = $(this).data('desc');
+        var title = $(this).data('name');
+        replaceDescription(title, description);
+        $('.bookGallery img').removeClass('bookBorder');
+        $(this).addClass('bookBorder');
+    });
+};
+
+var replaceDescription = function replaceDescription(bookTitle, bookDescription) {
+    // code that empties and appends the book description
+    $('.bookName').empty().append(bookTitle);
+    $('.bookDescription').empty().append('<p>' + bookDescription + '</p>');
+};
+
+// Home button to go back to main section
+$('.home').click(function () {
+    $('#content').hide();
+    $('#main').show();
+});
+
+$(document).ready(function () {
+    childrenApp.init();
+});
